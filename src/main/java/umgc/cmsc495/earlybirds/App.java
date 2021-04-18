43728376e3568c2f86f3ef5ff1aeb1418e430fd7
Xingyi Zhang <59896882+Xingyixzhang@users.noreply.github.com>
2021-04-18 16:46:55 -0700

    Update App.java

--- src/main/java/umgc/cmsc495/earlybirds/App.java
+++ src/main/java/umgc/cmsc495/earlybirds/App.java
@@ -1,5 +1,25 @@
+import java.io.FileReader;
+// import org.json.*;
+import org.json.simple.*;
+import org.json.simple.parser.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Parse JSON object:
+        JSONParser parser = new JSONParser();
+        JSONObject jsonObj = new JSONObject();
+
+        try {
+            // Opening the file
+            Object obj = parser.parse(new FileReader("WeeklyCases.json"));
+            // creating a Json object
+            jsonObj = (JSONObject) obj;
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+        LineChartVisual lineChart = new LineChartVisual(jsonObj);
+        BarChartVisual barChart = new BarChartVisual(jsonObj);
     }
 }
