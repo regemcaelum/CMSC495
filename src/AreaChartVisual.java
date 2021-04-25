f5fa4890e4e4809118a81167b6059badde3e9033
David Pierre <dpierre@thatcyberguy.com>
2021-04-24 20:39:35 -0400

    New updated files

---
+++ src/AreaChartVisual.java
@@ -0,0 +1,12 @@
+import java.io.FileReader;
+// import org.json.*;
+import org.json.simple.*;
+import org.json.simple.parser.*;
+
+public class AreaChartVisual {
+    private JSONObject obj;
+
+    public AreaChartVisual(JSONObject jsonObj) {
+        obj = jsonObj;
+    }
+}
---
+++ src/BarChartVisual.java
@@ -0,0 +1,12 @@
+import java.io.FileReader;
+// import org.json.*;
+import org.json.simple.*;
+import org.json.simple.parser.*;
+
+public class BarChartVisual {
+    private JSONObject obj;
+
+    public BarChartVisual(JSONObject jsonObj) {
+        obj = jsonObj;
+    }
+}
